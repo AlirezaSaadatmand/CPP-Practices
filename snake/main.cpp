16f8e9c6ed71dc6350d4388fa26be436c434ee30
Alireza <alirezasaadat056@gmail.com>
2024-09-21 01:02:18 +0330

    new files

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,43 +1,52 @@
 #include <iostream>
-#include <raylib.h>
+#include <conio.h>
+#include <windows.h>
 
-using namespace std;
+bool gameOver = false;
+const int width = 40;
+const int hieght = 20;
 
-int main () {
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
+void setUp(){
 
-    cout << "Hello World" << endl;
+}
+void draw(){
+    system("CLS");
+    for(int i = 0; i<width; i++){
+        std::cout << "#";
+    }std::cout << std::endl;
 
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
-
-    while (WindowShouldClose() == false){
-        BeginDrawing();
-        ClearBackground(BLACK);
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if(ball_x + ball_radius >= screenWidth  || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
+    for (int i = 0; i < hieght ; i++){
+        for (int j = 0; j < width ; j++){
+            if (j == 0 || j == width -1){
+                std::cout << "#";
+            }else{
+                std::cout << " ";
+            }
         }
-
-        if(ball_y + ball_radius >= screenHeight  || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-
-        DrawCircle(ball_x,ball_y,ball_radius, WHITE);
-        EndDrawing();
+            std::cout << std::endl;
     }
 
-    CloseWindow();
+    for(int i = 0; i<width; i++){
+        std::cout << "#";
+    }
+
+}
+void input(){
+
+}
+void logic(){
+
+}
+
+int main(){
+
+    setUp();
+    while (!gameOver){
+        draw();
+        input();
+        logic();
+        Sleep(200);
+    }
     return 0;
 }
